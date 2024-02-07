b2feda4e572093b71ea271635e69a12b2cb7fcdd
Keivan-sf <keyvan0082@gmail.com>
2024-02-07 10:37:34 +0330

    Move main code to index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,23 @@
-import { startServer } from "./handler";
+import { WebSocketServer } from "ws";
+import express from "express";
+import { WSConnection } from "./connection";
 
-startServer();
+export const startServer = () => {
+  const app = express();
+  const server2 = app.listen(9092);
+  const wss = new WebSocketServer({ server: server2 });
+  wss.on("connection", (ws) => {
+    const client = new WSConnection(ws);
+    client.on("data", (data) => {
+      console.log("client data:", data);
+      client.close();
+    });
+    client.on("close", () => {
+      console.log("wss client closed");
+    });
+  });
+
+  wss.on("listening", () => {
+    console.log("wss is listening on port 9092");
+  });
+};
