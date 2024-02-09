d65bfa3a46a5a4c38e9508195d6f67c401289dc0
Keivan-sf <keyvan0082@gmail.com>
2024-02-09 13:12:54 +0330

    Handle authentication error

--- client/index.ts
+++ client/index.ts
@@ -3,7 +3,21 @@ import { WebSocket } from "ws";
 const client = new WebSocket("ws://localhost:9092");
 client.on("open", () => {
   // const b = Buffer.from([0x01, 0x02, 0x65]);
-  // const b = Buffer.from([128, 4, 1, 2, 3, 7, 4, 1, 2, 3, 9]);
-  const b = Buffer.from([128]);
+  const uname = Buffer.from("admin");
+  const pw = Buffer.from("adminpw");
+  const b = Buffer.concat([
+    Buffer.from([128]),
+    Buffer.from([uname.length]),
+    uname,
+    Buffer.from([pw.length]),
+    pw,
+  ]);
+  console.log("sending ", b);
+  // const b = Buffer.from([128]);
   client.send(b);
+  client.send(b);
+});
+
+client.on("message", (data: Buffer) => {
+  console.log(data);
 });
--- server/Handler/handler.ts
+++ server/Handler/handler.ts
@@ -1,5 +1,13 @@
 import { Connection } from "../interfaces";
 
+const COMMANDS = {
+  AUTH: 128,
+};
+
+const ERRORS = {
+  BAD_UNAME_PW: 1,
+};
+
 export class Client {
   public processes: Map<number, Process> = new Map();
   constructor(public connection: Connection) {
@@ -9,7 +17,7 @@ export class Client {
         return;
       } else {
         try {
-          this.handleClientSpeceficRequest(data);
+          this.handleClientSpeceficCommand(data);
         } catch (err) {
           console.log("error", err);
         }
@@ -20,8 +28,8 @@ export class Client {
   public send(data: Buffer, pid?: number) {}
   public close(data: Buffer, pid?: number) {}
 
-  private handleClientSpeceficRequest(data: Buffer) {
-    if (data.at(0) == 128) {
+  private handleClientSpeceficCommand(data: Buffer) {
+    if (data.at(0) == COMMANDS.AUTH) {
       this.authenticate(data);
     }
   }
@@ -32,7 +40,13 @@ export class Client {
     const PWLEN = data.at(IDLEN + 2) as number;
     const PW = data.subarray(IDLEN + 3, PWLEN + IDLEN + 3);
     console.log(ID, PW);
-    this.connection.write(Buffer.from([128]));
+    if (ID.toString() == "admin" && PW.toString() == "adminpw") {
+      this.connection.write(Buffer.from([128]));
+    } else {
+      this.connection.write(
+        Buffer.from([0x00, COMMANDS.AUTH, ERRORS.BAD_UNAME_PW]),
+      );
+    }
   }
 }
 
