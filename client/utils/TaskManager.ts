1c656c0a40f9cdb46feb1500a796e0ac0e013146
Keivan-sf <keyvan0082@gmail.com>
2024-02-15 13:39:34 +0330

    Convert task manager class to methods

--- client/utils/TaskManager.ts
+++ client/utils/TaskManager.ts
@@ -1,38 +1,38 @@
 import { Duplex } from "stream";
 import { Task } from "./interfaces";
 
-export class TaskManager {
-  private unused_TIDs: Set<number> = new Set();
-  private used_TIDs: Set<number> = new Set();
-  public tasks: Task[] = [];
-  constructor() {
-    this.unused_TIDs = this.generateIDSet();
+let unused_TIDs: Set<number> = new Set();
+let used_TIDs: Set<number> = new Set();
+const tasks: Task[] = [];
+unused_TIDs = generateIDSet();
+
+export function createTask(): Task {
+  const task: Task = {
+    tid: getTID(),
+    incoming: new Duplex(),
+    outgoing: new Duplex(),
+  };
+  tasks.push(task);
+  return task;
+}
+
+export function freeTID(id: number) {
+  used_TIDs.delete(id);
+  unused_TIDs.add(id);
+}
+
+function getTID() {
+  if (unused_TIDs.size < 1) throw new Error("out of tids");
+  const val = unused_TIDs.values().next().value;
+  unused_TIDs.delete(val);
+  used_TIDs.add(val);
+  return val;
+}
+
+function generateIDSet(): Set<number> {
+  const set: Set<number> = new Set();
+  for (let i = 1; i < 65534; i++) {
+    set.add(i);
   }
-  createTask(): Task {
-    const task: Task = {
-      tid: this.getTID(),
-      incoming: new Duplex(),
-      outgoing: new Duplex(),
-    };
-    this.tasks.push(task);
-    return task;
-  }
-  public freeTID(id: number) {
-    this.used_TIDs.delete(id);
-    this.unused_TIDs.add(id);
-  }
-  private getTID() {
-    if (this.unused_TIDs.size < 1) throw new Error("out of tids");
-    const val = this.unused_TIDs.values().next().value;
-    this.unused_TIDs.delete(val);
-    this.used_TIDs.add(val);
-    return val;
-  }
-  private generateIDSet(): Set<number> {
-    const set: Set<number> = new Set();
-    for (let i = 1; i < 65534; i++) {
-      set.add(i);
-    }
-    return set;
-  }
+  return set;
 }
