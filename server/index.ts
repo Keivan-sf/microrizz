2eddd99a9edd9b930e53d390e99d281d34791fed
Keivan-sf <keyvan0082@gmail.com>
2024-02-18 15:36:29 +0330

    Add a home page to proxy server

--- server/index.ts
+++ server/index.ts
@@ -3,20 +3,24 @@ import express from "express";
 import { WSConnection } from "./connection";
 import { Client } from "./Handler/handler";
 import { NetworkImbalancer } from "./Utils/NetworkImbalancer";
+const PORT = 9092;
 
 export const startServer = () => {
   const app = express();
-  const server2 = app.listen(9092);
+  app.get("/", (req, res) => {
+    res.send("<html><h4>Work in progress...</h4></br>Coming soon</html>");
+  });
+  const server2 = app.listen(PORT);
   const wss = new WebSocketServer({ server: server2 });
   wss.on("connection", (ws) => {
     new Client(new WSConnection(ws));
   });
 
   wss.on("listening", () => {
-    console.log("wss is listening on port 9092");
+    console.log(`wss is listening on port ${PORT}`);
   });
   const imbalaner = new NetworkImbalancer(["http://time.ir"], 5000);
-  imbalaner.start();
+  // imbalaner.start();
 };
 
 startServer();
