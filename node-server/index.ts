ec291cf830f890f423035aa1af14ced9cf488821
Keivan-sf <keyvan0082@gmail.com>
2025-07-12 23:21:10 +0330

    server: implement a way for sending ice candidates to client via polling

--- node-server/index.ts
+++ node-server/index.ts
@@ -28,16 +28,25 @@ function routeWRTC(router: Router) {
     const answer = await client.getAnswer(req.body.offer);
     res.send({ id: req.body.id, answer });
   });
-  router.get("/ice-candidate", (req, res) => {});
-  router.post("/ice-candidate", (req, res) => {
+  router.post("/get-ice-candidate", (req, res) => {
     const client = wrtcClients.find((w) => w.id == req.body.id);
     if (!client) {
       res.statusCode = 400;
       res.send({ msg: `no client found with id ${req.body.id}` });
       return;
     }
+    res.send({ id: client.id, candidates: client.getIceCandidates() });
+  });
+  router.post("/send-ice-candidate", (req, res) => {
+    const client = wrtcClients.find((w) => w.id == req.body.id);
+    if (!client) {
+      res.statusCode = 400;
+      res.send({ msg: `no client found with id ${req.body.id}` });
+      return;
+    }
+    console.log("added new candidate from client")
     client.addCandidate(req.body.candidate);
-    res.send(200);
+    res.sendStatus(200);
   });
 }
 
