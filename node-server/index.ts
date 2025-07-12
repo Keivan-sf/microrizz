8b55733198380c8a3f9ea57acec53026cd4f61c7
Keivan-sf <keyvan0082@gmail.com>
2025-07-12 20:51:20 +0330

    server: create webrtc route structure

--- node-server/index.ts
+++ node-server/index.ts
@@ -1,5 +1,5 @@
 import { WebSocketServer } from "ws";
-import express from "express";
+import express, { Router } from "express";
 import { WSConnection } from "./connection";
 import { Client } from "./Handler/handler";
 import { NetworkImbalancer } from "./Utils/NetworkImbalancer";
@@ -8,11 +8,25 @@ config();
 const PORT = process.env.PORT ?? 3000;
 const TIME_OUT = 15000;
 
+function routeWRTC(router: Router) {
+  let id_counter = 0;
+  const wrtcClients = [];
+  router.get("/initiate", (req, res) => {
+    res.send({ id: ++id_counter });
+  });
+  router.post("/offer", (req, res) => {});
+  router.get("/ice-candidate", (req, res) => {});
+  router.post("/ice-candidate", (req, res) => {});
+}
+
 export const startServer = () => {
   const app = express();
   app.get("/", (req, res) => {
     res.send("<html><h4>Work in progress...</h4></br>Coming soon</html>");
   });
+  const wrtc_router = express.Router();
+  routeWRTC(wrtc_router);
+  app.use("/webrtc", wrtc_router);
   const server2 = app.listen(PORT);
   const wss = new WebSocketServer({ server: server2 });
   wss.on("connection", (ws) => {
