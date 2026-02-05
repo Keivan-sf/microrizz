55bb6aabd905075c72d6517f74c56a48a27a71dd
Keivan-sf <keyvan0082@gmail.com>
2026-02-05 12:48:22 +0330

    add chrome headers

--- client/lib/utils/HTTP.ts
+++ client/lib/utils/HTTP.ts
@@ -28,9 +28,25 @@ export class HTTPConnection implements Connection {
     const response = await axios.post(this.end_point, this.readable_stream, {
       responseType: "stream",
       headers: {
-        "Content-Type": "text/plain",
-        "Transfer-Encoding": "chunked",
+        "Content-Type": "application/octet-stream",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        Accept: "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive",
+        Pragma: "no-cache",
+        "Sec-Ch-Ua":
+          '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": '"Windows"',
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
       },
+      maxBodyLength: Infinity,
+      maxContentLength: Infinity,
     });
     response.data.on("data", (data: Buffer) => {
       console.log("getting data from server:", data);
