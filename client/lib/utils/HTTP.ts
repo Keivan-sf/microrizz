ec8a244d020fa9c2e9c3cec037ea6fb0e6c9f3a2
Keivan-sf <keyvan0082@gmail.com>
2026-02-05 17:30:35 +0330

    Use separate requests for send/receive on http

--- client/lib/utils/HTTP.ts
+++ client/lib/utils/HTTP.ts
@@ -34,7 +34,21 @@ export class HTTPConnection implements Connection {
     });
     console.log("headers:", form.getHeaders());
 
-    const response = await axios.post(this.end_point, form, {
+    const response = await axios.get(this.end_point, {
+      responseType: "stream",
+      headers: {
+        "Content-Type": "application/octet-stream",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        Accept: "*/*",
+      },
+    });
+    response.data.on("data", (data: Buffer) => {
+      console.log("getting data from server:", data);
+      this.emitter.emit("data", data);
+    });
+
+    axios.post(this.end_point, form, {
       responseType: "stream",
       headers: {
         "Content-Type": "application/octet-stream",
@@ -46,10 +60,7 @@ export class HTTPConnection implements Connection {
       maxBodyLength: Infinity,
       maxContentLength: Infinity,
     });
-    response.data.on("data", (data: Buffer) => {
-      console.log("getting data from server:", data);
-      this.emitter.emit("data", data);
-    });
+
   }
 
   write(data: Buffer): void {
--- node-server/Lib/HTTP.ts
+++ node-server/Lib/HTTP.ts
@@ -9,15 +9,20 @@ export class HTTPConnection implements Connection {
   constructor(router: Router, end_point: string) {
     router.post(`/${end_point}`, (req, res) => {
       this.req = req;
-      this.res = res;
       this.req.on("data", (data: Buffer) => {
         this.emitter.emit("data", data);
         console.log("received from client:", data);
       });
     });
+
+    router.get(`/${end_point}`, (req, res) => {
+      console.log("get request received");
+      this.res = res;
+    });
   }
   write(data: Buffer): void {
     this.res?.write(data);
+    console.log("wrote")
   }
   on(type: string, cb: any): void {
     this.emitter.on(type, cb);
